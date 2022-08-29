b3ff968b8667f6f3dd48840f2a9f5453d87dfdbf
danpeacock <danpeacock96@gmail.com>
2022-08-29 22:31:34 +0100

    add conditional aws

--- main.tf
+++ main.tf
@@ -1,1 +1,43 @@
-#test
+data "terraform_remote_state" "creds" {
+  backend = "remote"
+  config = {
+    organization = "danpeacock"
+    workspaces = {
+      name = "Hargreaves-Lansdown-Vault-Admin"
+    }
+  }
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = data.terraform_remote_state.creds.outputs.backend
+  role    = data.terraform_remote_state.creds.outputs.role
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create AWS EC2 Instance
+resource "aws_instance" "main" {
+  count = var.aws ? 1 : 0
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.nano"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "name" { 
+    default = "dynamic-aws-creds-operator" 
+    }
+variable "region" { 
+    default = "us-east-1" 
+    }
+variable "path" {
+     default = "../vault-admin-workspace/terraform.tfstate" 
+     }
+variable "ttl" { 
+    default = "1" 
+    }
+
+variable "aws" {
+    default = "False"  
+}
